a19704e69ae12ed1bd164ebca0511290c4a41e31
Quinn <qtoconnor@gmail.com>
2017-11-22 00:34:53 -0600

    Various changes

--- src/os/Output.java
+++ src/os/Output.java
@@ -1,40 +1,56 @@
 package os;
 
 import java.util.*;
+import java.util.concurrent.Semaphore;
+
+import model.Guard;
 
 
 public class Output extends Thread{
-	//
+	//--------------------------------------------------
 	// DATA
-	//
+	//--------------------------------------------------
+	
+	// Process info
+	Semaphore sem = new Semaphore(1);
+	// p1
+	String p1name = "P1";
+	int p1mem = 64;
+	double[] p1btimes = {3, .14, -2, 2};
+	// p2
+	String p2name = "P2";
+	int p2mem = 32;
+	double[] p2btimes = {5, .07, 1, -2, 1};
+	// p3
+	String p3name = "P3";
+	int p3mem = 32;
+	double[] p3btimes = {6};
+	
+	Process p1 = new Process(p1name, p1mem, p1btimes, sem);
+	Process p2 = new Process(p2name, p2mem, p2btimes, sem);
+	Process p3 = new Process(p3name, p3mem, p3btimes, sem);
 	
 	// CPU
+	String cpuMSG;
 	int time = 0;
-	String currentP;
-	String[] readyQ = {"poop"}; //should be process list
-	String[] ioQ = {"butts"};
+	Process currentP;
+	List<Process> readyQ = new LinkedList<Process>();
+	List<Process> ioQ = new LinkedList<Process>();
 	
 	// MEM
-	//Process[] loaded;
+	List<Process> loaded = new LinkedList<Process>();
 	int totalSpace = 512;
 	int usedSpace = 0;
 	int avalSpace = totalSpace;
 	
+	// General
+	List<Process> pList = new LinkedList<Process>();
+
 	
-	String p1name = "P1";
-	String p2name = "P2";
-	int mem = 32;
-	int[] cputime = {2,3,4,0};
-	int[] iotime = {3,4,0,0};
-	Process p1 = new Process(p1name, mem, cputime, iotime);
-	Process p2 = new Process(p2name, mem, cputime, iotime);
 	
-	Process[] processList = {p1, p2};
-	Process[] loaded = {p1, p2};
-	
-	//
+	//--------------------------------------------------
 	// METHODS
-	//
+	//--------------------------------------------------
 	
 	public void updateUsedSpace() {
 		int updated = 0;
@@ -44,38 +60,57 @@ public class Output extends Thread{
 		usedSpace = updated;
 	}
 	
-	public String processListToString(Process[] list) {
-		String[] array = new String[list.length];
-		int i = 0;
-		for(Process p : list) {
-			array[i] = p.name;
-			i++;
+	
+	// Takes in a process queue and returns a string to be printed
+	public String processListToString(List<Process> list) {
+		if(list.size() == 0) {
+			return "-";
+		}else {
+			String[] array = new String[list.size()];
+			int i = 0;
+			for(Process p : list) {
+				array[i] = p.name;
+				i++;
+			}
+			return Arrays.toString(array);
 		}
-		return Arrays.toString(array);
 	}
 	
+	public void addP () {
+		pList.add(p2);
+	}
+	
+	//--------------------------------------------------
+	// THREAD
+	//--------------------------------------------------
 	public void run() {
-		
-		for(Process p : processList) {
+		pList.add(p1);
+		pList.add(p2);
+		pList.add(p3);
+		cpuMSG = p1.name;
+		int j = pList.size();
+		for(int i=0; i < j; i++) { // Need to change
+			currentP = pList.get(i);
+			currentP.run();
+			
 			//time
 			System.out.println("@time: " + time);
-			time = p.cpuBurst[0] + time;
+			time = currentP.time + time;
 			
 			//cpu usage
-			currentP = p.name;
-			System.out.println("CPU Usage: " + currentP + " Running");
-			
-			//cpu queues
-			System.out.println("Ready Queue: " + readyQ[0]);
-			System.out.println("I/O Waiting Queue: " + ioQ[0]);
+			System.out.println("CPU Usage: " + cpuMSG + " Running");
+			System.out.println("	Ready Queue: " + processListToString(readyQ));
+			System.out.println("	I/O Waiting Queue: " + processListToString(ioQ));
 			
 			//mem
-			System.out.println("Loaded: " + processListToString(loaded));
+			System.out.println("Memory Usage:");
+			System.out.println("	Loaded: " + processListToString(loaded));
 			avalSpace = totalSpace - usedSpace;
-			System.out.println("Used Space: " + usedSpace);
-			System.out.println("Available Space: " + avalSpace);
+			System.out.println("	Used Space: " + usedSpace);
+			System.out.println("	Available Space: " + avalSpace);
+			System.out.println();
 			
-			
+			cpuMSG = currentP.name;
 			
 		}
 		
@@ -85,9 +120,8 @@ public class Output extends Thread{
 	
 
 	public static void main(String[] args) {
-		Output poop = new Output();
-		poop.start();
-		System.out.println("Starting poop");
+		Output test = new Output();
+		test.start();
 
 	}
 
--- src/os/Process.java
+++ src/os/Process.java
@@ -1,25 +1,51 @@
 package os;
 
-public class Process{
+import java.util.concurrent.Semaphore;
+
+public class Process extends Thread{
+	// Process Data
 	String name;
-	int reqMem;
-	int[] cpuBurst;
-	int[] ioBurst;
-	int s = 0;
+	int reqMem;	
+	double[] btimes;
 	
+	int time = 0;
+	int i = 0;
+	
+	// Semaphore
+	Semaphore sem;
 	
 	// Critical sections are negative values in the burst time arrays
+	//int[] cpuBurstTime, int[] ioBurstTime
 	
-	public Process(String name, int mem, int[] cpuBurstTime, int[] ioBurstTime) {
+	public Process(String name, int mem, double[] bursttimes, Semaphore sem) {
 		this.name = name;
 		this.reqMem = mem;
-		this.cpuBurst = cpuBurstTime;
-		this.ioBurst = ioBurstTime;
+		//this.cpuBurst = cpuBurstTime;
+		//this.ioBurst = ioBurstTime;
+		this.btimes = bursttimes;
 	}
 	
-	
-	
-	
+	public void run() {
+		if(btimes[i] >= 1) { 
+		// CPU Burst Time
+			time = time + (int) btimes[i];
+			
+		}else if(btimes[i] > 0) { 
+		// IO Burst Times
+			time = time + (int)(btimes[i] * 100);
+			
+		}else if(btimes[i] < 0) { 
+		// Critical Section
+			time = time + (int)Math.abs(btimes[i]);
+			
+		}else {
+			// Something went wrong
+			System.out.println("burst time error");
+		}
+		
+		i++;
+		
+	}
 	
 	
 }

9324cf3b110ffb6d7a43c8782a21bbefc79bd1ea
Quinn <qtoconnor@gmail.com>
2017-11-29 13:18:54 -0600

    finished output

--- src/os/Output.java
+++ src/os/Output.java
@@ -21,7 +21,7 @@ public class Output extends Thread{
 	// p1
 	String p1name = "P1";
 	int p1mem = 64;
-	double[] p1btimes = {3, .14, -2, 2};
+	double[] p1btimes = {3, .14, 3, -2, 2};
 	// p2
 	String p2name = "P2";
 	int p2mem = 32;
@@ -37,20 +37,24 @@ public class Output extends Thread{
 	
 	// CPU
 	String cpuMSG;
-	int time = 0;
+	static int time = 0;
+//	int endtime = 0;
 	Process currentP;
-	List<Process> readyQ = new LinkedList<Process>();
-	List<Process> ioQ = new LinkedList<Process>();
+	Queue<Process> readyQ = new LinkedList<Process>();
+	Queue<Process> waitingQ = new LinkedList<Process>();
 	
 	// MEM
 	List<Process> loaded = new LinkedList<Process>();
-	int totalSpace = 512;
+	int totalSpace = 256;
 	int usedSpace = 0;
 	int avalSpace = totalSpace;
 	
 	// General
 	List<Process> pList = new LinkedList<Process>();
-
+	int j;	// set to the pList size in run()
+	String termMsg = "";
+	String idleMsg = "";
+	Boolean sysIdle = false;
 	
 	
 	//--------------------------------------------------
@@ -59,10 +63,11 @@ public class Output extends Thread{
 	
 	public void updateUsedSpace() {
 		int updated = 0;
-		for(Process l : loaded) {
-			usedSpace = l.reqMem + usedSpace;
+		for(Process p : loaded) {
+			updated = p.reqMem + updated;
 		}
 		usedSpace = updated;
+		avalSpace = totalSpace - updated;
 	}
 	
 	
@@ -81,10 +86,95 @@ public class Output extends Thread{
 		}
 	}
 	
-	public void addP () {
-		pList.add(p2);
+	public String processQueueToString(Queue<Process> Q) {
+		if(Q.size() == 0) {
+			return "-";
+		}else {
+			String[] array = new String[Q.size()];
+			int i = 0;
+			for(Process p : Q) {
+				array[i] = p.name;
+				i++;
+			}
+			return Arrays.toString(array);
+		}
 	}
 	
+	
+//	public String isRunning() {
+//		if(currentP.idle == true) {
+//			return "idle ";
+//		}else {
+//			return currentP.name + " running ";
+//		}
+//	}
+	
+	public String inCS() {
+		if(currentP.cs == true) {
+			return "(in CS) ";
+		}else {
+			return "";
+		}
+	}
+	
+
+	
+	public void updateTime() {
+		//time
+		System.out.println("@time: " + time);
+		time = currentP.bTime + time;
+	}
+	
+	public void updateCPU() {
+		//cpu usage
+		System.out.println("CPU Usage: "+ currentP.name + " running " + inCS() + termMsg);
+		System.out.println("	Ready Queue: " + processQueueToString(readyQ));
+		System.out.println("	I/O Waiting Queue: " + processQueueToString(waitingQ));
+		
+		cpuMSG = "";
+		termMsg = "";
+	}
+	
+	public void updateMem() {
+		//updates usedSpace
+		int updated = 0;
+		for(Process p : loaded) {
+			updated = p.reqMem + updated;
+		}
+		usedSpace = updated;
+		avalSpace = totalSpace - updated;
+		
+		//mem		
+		System.out.println("Memory Usage:");
+		System.out.println("	Loaded: " + processListToString(loaded));
+//		avalSpace = totalSpace - usedSpace;
+		System.out.println("	Used Space: " + usedSpace);
+		System.out.println("	Available Space: " + avalSpace);
+		System.out.println();
+	}
+	
+	public void updateQs() {
+		for(Process p : pList) {
+			if(p.flag == "CPU" || p.flag == "CS" && !readyQ.contains(p)) {
+				readyQ.add(p);
+			} else if(p.flag == "IO" && !waitingQ.contains(p)){
+				waitingQ.add(p);
+				p.ioStartTime = time;
+				p.ioEndTime = p.ioTime + p.ioStartTime;
+
+			}
+		}
+	}
+	
+	public void checkWaitingQ() {
+		for(Process p : waitingQ) {
+			if(time >= p.ioEndTime) {
+				readyQ.add(p);
+				waitingQ.remove(p);
+				p.ioTime = 0;
+			}
+		}
+	}
 	
 	//--------------------------------------------------
 	// THREAD
@@ -93,34 +183,95 @@ public class Output extends Thread{
 		pList.add(p1);
 		pList.add(p2);
 		pList.add(p3);
-		cpuMSG = p1.name + " running";
-		int j = pList.size();
 		
-		for(int i=0; i < j; i++) { 
-			currentP = pList.get(i);
-			currentP.run();
-			
-			//time
-			System.out.println("@time: " + time);
-			time = currentP.time + time;
-			
-			//cpu usage
-			System.out.println("CPU Usage: " + cpuMSG);
-			System.out.println("	Ready Queue: " + processListToString(readyQ));
-			System.out.println("	I/O Waiting Queue: " + processListToString(ioQ));
-			
-			//mem
-			System.out.println("Memory Usage:");
-			System.out.println("	Loaded: " + processListToString(loaded));
-			avalSpace = totalSpace - usedSpace;
-			System.out.println("	Used Space: " + usedSpace);
-			System.out.println("	Available Space: " + avalSpace);
-			System.out.println();
-			
-			cpuMSG = currentP.name + "running";
-			
-		}
+		readyQ.add(p1);
+		readyQ.add(p2);
+		readyQ.add(p3);
 		
+		
+		loaded.add(p1);
+		loaded.add(p2);
+		loaded.add(p3);
+		
+		
+		while(true) {		
+			
+			
+			if(!readyQ.isEmpty()) {
+				currentP = readyQ.remove();
+				cpuMSG = currentP.name + inCS() + termMsg;
+				currentP.run();
+				
+				//time
+				updateTime();
+				
+				//cpu usage
+				updateCPU();
+				
+				//mem
+				updateMem();
+				
+				updateQs();
+				
+				
+				if(currentP.terminated) {
+					termMsg = "- " + currentP.name + " terminated";
+					loaded.remove(currentP);
+//					pList.remove(currentP);
+				}
+				
+				
+			} else if(!waitingQ.isEmpty()) {
+				sysIdle = true;
+				cpuMSG = "idle " + termMsg;
+				if(termMsg.length() > 1) {
+					//time
+					System.out.println("@time: " + time);
+					
+					//cpu usage
+					System.out.println("CPU Usage: "+ cpuMSG);
+					System.out.println("	Ready Queue: " + processQueueToString(readyQ));
+					System.out.println("	I/O Waiting Queue: " + processQueueToString(waitingQ));
+					
+					//mem
+					updateMem();
+					
+					updateQs();
+					termMsg = "";
+				}
+				
+				time++;
+				//checkWaitingQ();
+				
+				if(time == 100) {
+					System.out.print("time = 100, ending program");
+					break;
+				}
+				
+			} else {
+				//time
+				System.out.println("@time: " + time);
+				
+				//cpu usage
+				System.out.println("CPU Usage: "+ termMsg);
+				System.out.println("	Ready Queue: " + processQueueToString(readyQ));
+				System.out.println("	I/O Waiting Queue: " + processQueueToString(waitingQ));
+				
+				//mem
+				updateMem();
+				
+				System.out.println("\n" + "********************" + "\n" + "END OF OS SIMULATION" + "\n" + "********************");
+				break;
+			}
+			
+			checkWaitingQ();
+			
+			
+
+			currentP = null;
+			
+			
+		}	
 	}
 	
 	
@@ -129,7 +280,8 @@ public class Output extends Thread{
 	public static void main(String[] args) {
 		Output test = new Output();
 		test.start();
-
+		
+		
 	}
 
 }
--- src/os/Process.java
+++ src/os/Process.java
@@ -7,9 +7,17 @@ public class Process extends Thread{
 	String name;
 	int reqMem;	
 	double[] btimes;
+	String flag;
 	
 	int time = 0;
+	int bTime = 0;
+	int ioTime = 0;
+	int ioStartTime = 0;
+	int ioEndTime = 0;
 	int i = 0;
+	Boolean terminated = false;
+	Boolean idle = false;
+	Boolean cs = false;
 	
 	// Semaphore
 	Semaphore sem;
@@ -21,27 +29,78 @@ public class Process extends Thread{
 		this.btimes = bursttimes;
 	}
 	
-	public void run() {
-		if(btimes[i] >= 1) { 
-		// CPU Burst Time (positive integers)
-			time = time + (int) btimes[i];
-			
-		}else if(btimes[i] > 0) { 
-		// IO Burst Times (positive decimals)
-			time = time + (int)(btimes[i] * 100);
-			
-		}else if(btimes[i] < 0) { 
-		// Critical Section (negative integers)
-			time = time + (int)Math.abs(btimes[i]);
-			
-		}else {
-			// Something went wrong
-			System.out.println("burst time error");
+	public void checkTime() {
+		if(Output.time >= this.time) {
+			idle = false;
 		}
-		
-		i++;
-		
+	}
+	
+	public void updateFlag() {
+		if (btimes[i] >= 1) {
+			// CPU Burst Time (positive integers)
+			flag = "CPU";
+
+		} else if (btimes[i] > 0 && btimes[i] < 1.0) {
+			// IO Burst Time (positive decimals)
+			ioTime = (int) (btimes[i] * 100);
+			flag = "IO";
+			i++;
+
+		} else if (btimes[i] < 0) {
+			// Critical Section (negative integers)
+			flag = "CS";
+
+		} else {
+			// Something went wrong
+			System.out.println("flag Error");
+		}
 	}
 	
 	
+	public void run() {
+		checkTime();
+
+			cs = false;
+			idle = false;
+			flag = "";
+		
+			
+			
+			if (btimes[i] >= 1) {
+				// CPU Burst Time (positive integers)
+				time = time + (int) btimes[i];
+				bTime = (int) btimes[i];
+
+			} else if (btimes[i] > 0 && btimes[i] < 1.0) {
+				// IO Burst Time (positive decimals)
+				time = time + (int) (btimes[i] * 100);
+				bTime = (int) (btimes[i] * 100);
+				idle = true;
+
+			} else if (btimes[i] < 0) {
+				// Critical Section (negative integers)
+				time = time + (int) Math.abs(btimes[i]);
+				bTime = (int) Math.abs(btimes[i]);
+				cs = true;
+
+			} else {
+				// Something went wrong
+				System.out.println("burst time error");
+			}
+			
+			i++;
+
+			
+
+			if (btimes.length == i) {
+				terminated = true;
+				
+			} 
+			else {
+				updateFlag();
+			}
+
+			
+				
+	}	
 }
